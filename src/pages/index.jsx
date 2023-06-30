d782f51611725813b53c5806e751321a0237864c
Atharv Vani <atharvavani110@gmail.com>
2023-06-30 12:14:08 +0530

    Init

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MobileAppLight from "../pages/mobile-app/mobile-app-light"
+
+const Home = () => {
+  return (
+    <>
+      <MobileAppLight />
+    </>
+  );
+};
+
+export default Home;
